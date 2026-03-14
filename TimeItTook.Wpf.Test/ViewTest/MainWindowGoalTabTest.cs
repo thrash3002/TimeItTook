a5ca95cabbaf94ce763a5eb785d8469b2d3e572c
Mamun Khan <mamun.khan@outlook.com>
2026-03-14 17:43:22 +1100

    Add test stubs to MainWindowGoalTabTest.cs

--- TimeItTook.Wpf.Test/ViewTest/MainWindowGoalTabTest.cs
+++ TimeItTook.Wpf.Test/ViewTest/MainWindowGoalTabTest.cs
@@ -13,5 +13,173 @@
         {
 
         }
+
+        [Fact]
+        public void ClickingAddGoalButtonShowsCreateGoalModal()
+        {
+
+        }
+
+        [Fact]
+        public void CanSeeSelectedGoalInSelectedGoalSection()
+        {
+
+        }
+
+        [Fact]
+        public void SelectedGoalReplacesPreviouslySelectedGoalInSelectedGoalSection()
+        {
+
+        }
+
+        [Fact]
+        public void SelectedGoalSectionShowsNextFiveTasks()
+        {
+
+        }
+
+        [Fact]
+        public void ClickingCopyToGoalStructureButtonShowsCreateGoalTypeModalWithRelevantInfoCopied()
+        {
+
+        }
+
+        [Fact]
+        public void DoubleClickingTaskFromSelectedGoalSectionOpensTaskTabWithTaskShowingInActiveTaskSection()
+        {
+
+        }
+
+        [Fact]
+        public void EditButtonOpensEditGoalDialog()
+        {
+
+        }
+
+        [Fact]
+        public void DeleteTaskFromGoalButtonGreyedOutUnlessTaskSelected()
+        {
+
+        }
+
+        [Fact]
+        public void RemoveTaskFromGoalButtonGreyedOutUnlessTaskSelected()
+        {
+
+        }
+
+        [Fact]
+        public void DeleteTaskFromGoalButtonOpensAreYouSureConfirmationMessageBox()
+        {
+
+        }
+
+        [Fact]
+        public void RemoveTaskFromGoalButtonOpensAreYouSureConfirmationMessageBox()
+        {
+
+        }
+
+        [Fact]
+        public void DeleteButtonOpensAreYouSureConfirmationMessageBox()
+        {
+
+        }
+
+        [Fact]
+        public void CanSee20GoalsInGoalList()
+        {
+
+        }
+
+        [Fact]
+        public void CompletedGoalsHiddenInGoalListIfShowCompletedCheckBoxIsUnchecked()
+        {
+
+        }
+
+        [Fact]
+        public void IsCompletedColumnHiddenInGoalListIfShowCompletedCheckBoxIsUnchecked()
+        {
+
+        }
+
+        [Fact]
+        public void CompletedGoalsShowInGoalListIfShowCompletedCheckBoxIsChecked()
+        {
+
+        }
+
+        [Fact]
+        public void IsCompletedColumnShowsInGoalListIfShowCompletedCheckBoxIsChecked()
+        {
+
+        }
+
+        [Fact]
+        public void GoalListCanBeFilteredByGoalType()
+        {
+
+        }
+
+        [Fact]
+        public void GoalListCanBeFilteredToShow10GoalsOnly()
+        {
+
+        }
+
+        [Fact]
+        public void GoalListCanBeFilteredToShow25GoalsOnly()
+        {
+
+        }
+
+        [Fact]
+        public void GoalListCanBeFilteredToShow50GoalsOnly() // TODO: is this necessary?
+        {
+
+        }
+
+        [Fact]
+        public void GoalListShowsAllIncompleteGoalsOnlyByDefault() // i.e. no other filters
+        {
+
+        }
+
+        [Fact]
+        public void GoalsAreOrderedByDeadlineByDefault()
+        {
+
+        }
+
+        [Fact]
+        public void GoalsAreOrderedByDeadlineAndThenPriorityByDefault()
+        {
+
+        }
+
+        [Fact]
+        public void ClickNameColumnHeaderToOrderByName()
+        {
+
+        }
+
+        [Fact]
+        public void ClickDeadlineColumnHeaderToReturnToDefaultOrder()
+        {
+
+        }
+
+        [Fact]
+        public void ClickPriorityColumnHeaderToOrderByPriority()
+        {
+
+        }
+
+        [Fact]
+        public void ClickIsCompletedColumnHeaderToOrderByIsCompleted()
+        {
+
+        }
     }
 }
